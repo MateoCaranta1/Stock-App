164a7ae97c2747a6c3c22520481999a4ffc7a7a0
mateocaranta <mateocaranta@gmail.com>
2025-07-14 19:21:54 -0300

    New user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,49 @@
+const userService = require('../services/user.service');
+
+exports.getAll = async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: 'Erro al obtener los usuarios.' });
+    }
+};
+
+exports.getByEmail = async (req, res) => {
+    try {
+        const user = await userService.getUserByEmail(req.params.email);
+        if(!user) return res.status(404).json({ error: 'Usuario no encontrado.' });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: 'Error al obtener usuario.' });
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        if (!user) return res.status(404).json({ error: 'Usuario no encontrado.' });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: 'Error al obtener usuario.' });
+    }
+};
+
+exports.create = async (req, res) => {
+    try {
+        const user = await userService.createUser(req.body);
+        res.status(201).json(id);
+    } catch (err) {
+        res.status(500).json({ error: 'Error al crear usuario.' });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const user = await userService.login(email, password);
+        if (!user) return res.status(404).json({ error: 'Credenciales incorrectas.' });
+        res.status(201).json({ message: 'Bienvenido.' });
+    } catch (err) {
+        res.status(500).json({ error: 'Error al iniciar sesión.' });
+    }
+};
