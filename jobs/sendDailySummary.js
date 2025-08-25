64a7b236b8cd84f34bf91c9853bdebc85cf27a10
mateocaranta <mateocaranta@gmail.com>
2025-08-25 14:35:52 -0300

    Delete emailing service

--- jobs/sendDailySummary.js
+++
@@ -1,9 +0,0 @@
-const { summary } = require("../services/summary.service");
-const { sendMail } = require("../utils/mailer");
-
-const runDailyJob = async () => {
-   const resumen = await summary();
-   await sendMail('Resumen diario de stock', resumen);
-}
-
-runDailyJob();
